b5102f765ecd74970bf19596a5285ac7f07f2104
who._.hs <108789695+ChoeHyeongSoo@users.noreply.github.com>
2026-02-09 13:17:09 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 180 ms, Memory: 18368 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+class Main { // 알파벳 개수 카운팅 문제
+
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+
+        String line = sc.next();
+
+        int[] ans = new int[26];
+        for (int i = 0; i < line.length(); i++) {
+            char c = line.charAt(i);
+            ans[c-'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            sb.append(ans[i] + " ");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+}
