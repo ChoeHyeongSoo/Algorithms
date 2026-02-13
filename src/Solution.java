b39e21e5d8916aff73570a444effac5cf8cc2451
Choi HyeongSoo <sam000208@naver.com>
2026-02-13 11:59:24 +0900

    Modify Basic Soltion class

--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,25 @@
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 class Solution {
+    /*
 
+     */
     public static void main(String args[]) throws Exception {
 //        System.setIn(new FileInputStream("res/sample_input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T = sc.nextInt();
+//        Scanner sc = new Scanner(System.in);
+        int T = Integer.parseInt(br.readLine());
 
         for (int test_case = 1; test_case <= T; test_case++) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int l = Integer.parseInt(st.nextToken());
 
-
-
-//            System.out.println("#" + test_case + " " + ans);
+//            System.out.println("#" + test_case + " " + );
         }
-
     }
+
 }
