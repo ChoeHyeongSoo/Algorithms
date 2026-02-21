0afc7aff6b0cfc3ef84095de1f9db17b445d47b7
who._.hs <108789695+ChoeHyeongSoo@users.noreply.github.com>
2026-02-21 18:21:37 +0900

    2026 Feb 2nd week [D6_SW_Application_10th_WorkOrder] - Solve problem using BFS(Topological Sort) ans DFS, Compare each others sequence - Implement graph as List Array

--- src/Algorithm/Graph/DFS/D6_SW_Application_10th_WorkOrder_DFS.java
+++ src/Algorithm/Graph/DFS/D6_SW_Application_10th_WorkOrder_DFS.java
@@ -1,11 +1,11 @@
-package Algorithm.Graph.BFS;
+package Algorithm.Graph.DFS;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.*;
 
-class D6_SW_Application_10th_WorkOrder_BFS {
+class D6_SW_Application_10th_WorkOrder_DFS {
     /*
     해야 할 V개의 작업이 있다. 이들 중에 어떤 작업은 특정 작업이 끝나야 시작할 수 있으며, 이를 선행 관계라 하자.
     이런 작업의 선행 관계를 나타낸 그래프가 주어진다. 이 그래프에서 각 작업은 하나씩의 정점으로 표시되고 선행 관계는 방향성을 가진 간선으로 표현된다.
@@ -27,7 +27,7 @@ class D6_SW_Application_10th_WorkOrder_BFS {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int vertex = Integer.parseInt(st.nextToken());
             int edge = Integer.parseInt(st.nextToken());
-            int[] in_degree = new int[vertex+1];
+            boolean[] visit = new boolean[vertex+1];
             List<Integer>[] graph = new ArrayList[vertex+1];
 
             for (int i = 1; i <= vertex; i++)
@@ -37,34 +37,33 @@ class D6_SW_Application_10th_WorkOrder_BFS {
             for (int i = 0; i < edge; i++) {
                 int v = Integer.parseInt(st.nextToken());
                 int c = Integer.parseInt(st.nextToken());
-                in_degree[c]++;
                 graph[v].add(c);
             }
 
-            Queue<Integer> q = new LinkedList<>();
+            Stack<Integer> s = new Stack<>();
             StringBuilder sb = new StringBuilder();
 
             for (int idx = 1; idx <= vertex; idx++) {
-                if (in_degree[idx]==0)  // 바로 가능한 작업 큐에 삽입
-                    q.offer(idx);
+                if (!visit[idx]) dfs(idx, visit, s, graph);
             }
 
-            while (!q.isEmpty()) {
+            while (!s.isEmpty())
+                sb.append(s.pop() + " ");
 
-                int tmp = q.poll();
-
-                for (int c : graph[tmp]) {
-                    in_degree[c]--;         // 간선 제거
-
-                    if (in_degree[c]==0)    // 선행작업이 0이 되면 큐에 삽입
-                        q.offer(c);
-                }
-
-                sb.append(tmp + " ");
-            }
 
             System.out.println("#" + test_case + " " + sb);
         }
+    }
+
+    public static void dfs(int idx, boolean[] visit, Stack<Integer> s, List<Integer>[] graph) {
+        visit[idx] = true;
+
+        for (int c : graph[idx]) {
+            if (!visit[c])
+                dfs(c, visit, s, graph);
+        }
+
+        s.push(idx);
     }
 
 }
