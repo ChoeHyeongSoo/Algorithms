cab55a33615b7da4cad1d4ee372e67441950772f
who._.hs <108789695+ChoeHyeongSoo@users.noreply.github.com>
2026-02-24 22:15:13 +0900

    Basic Solution Template

--- src/Solution.java
+++ src/Solution.java
@@ -7,16 +7,12 @@ class Solution {
 
      */
     public static void main(String args[]) throws Exception {
-//        System.setIn(new FileInputStream("res/sample_input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-//        Scanner sc = new Scanner(System.in);
         int T = Integer.parseInt(br.readLine());
 
         for (int test_case = 1; test_case <= T; test_case++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
-            int l = Integer.parseInt(st.nextToken());
 
 //            System.out.println("#" + test_case + " " + );
         }
