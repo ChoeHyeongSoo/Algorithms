1d833b6b63abcb90f647ab4a3c138cbd07da20cd
who._.hs <108789695+ChoeHyeongSoo@users.noreply.github.com>
2026-02-27 00:57:49 +0900

    JDK 8: addFirst x -> DataStructure change : Deque/LinkedList
    -> Fink Exception

--- src/aaa.java
+++ src/aaa.java
@@ -16,29 +16,65 @@ class Solution {
             int k = Integer.parseInt(st.nextToken());
             StringBuilder sb = new StringBuilder(br.readLine());
 
-            // 순환 - 선형자료구조 : 덱
-            ArrayList<Character> arr = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                sb.charAt(i);
-            }
-
-
+            // 순환 - 선형자료구조 : 덱/LinkList-ArrayList : 성능비교
+            LinkedList<Character> arr = new LinkedList<>();
+            for (int i = 0; i < n; i++)
+                arr.add(sb.charAt(i));
             // 순위-중복 금지 -> 세트에 저장해서 내림차순 정렬
-            Set<Character> rank = new HashSet<>(); // 배열에 옮기며 정렬
+            Set<String> pw = new HashSet<>(); // 배열에 옮기며 정렬
+            int w = n/4; // pw 길이
+            save(arr, pw, w);
+            for (int i = 0; i < w; i++)  // 3번 회전
+                rotate_save(arr, pw, w);
 
-            int w = n/4; // 16진수 길이
-            for (int i = 0; i < 4; i++) {   // 4번만 반복
-
-            }
-
+            // 정렬 및 k번째 반환
+            int ans = get_pw(pw, k);
             // 10진수 변환 : 정답
 
-//            System.out.println("#" + test_case + " " + );
+            System.out.println("#" + test_case + " " + ans);
         }
     }
-}
 
-// 16->2진수 ; 각 자리 8^i * arr[i]
-public static int convert() {
+    // 회전
+    public static void rotate_save(LinkedList<Character> arr, Set<String> pw, int w) {
+        arr.addFirst(arr.getLast());
+        arr.removeLast();
+        save(arr, pw, w);
+    }
 
+    // 비밀번호 저장
+    public static void save(LinkedList<Character> arr, Set<String> pw, int w) {
+        for (int i = 0; i < 4; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int idx = 0; idx < w; idx++)
+                sb.append(arr.get(w*i+idx));
+            pw.add(sb.toString());
+        }
+    }
+
+    // 정렬 : 각 자리 char 크기 비교 / 비교해서 크면 앞쪽으로, 같으면 다음 자리 비교
+    public static int get_pw(Set<String> pw, int k) {
+        int[] rank = new int[pw.size()];
+        convert(pw, rank);
+        Arrays.sort(rank);
+
+        k = pw.size() - k;
+        return rank[k];
+    }
+
+    // 16->2진수 ; 각 자리 8^i * arr[i]
+    public static void convert(Set<String> pw, int[] arr) {
+        // c - '0', A~F는 - 'A' +10 : "ABCEF".contains(c) <-valueOf(c)?
+        int idx = 0;
+        for (String s : pw) {
+            int tmp = 0;
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if ("ABCEF".contains(String.valueOf(c)))
+                    tmp += (c - 'A' + 10)*(1<<(4*(s.length()-1-i)));
+                else tmp += (c - '0')*(1<<(4*(s.length()-1-i)));
+            }
+            arr[idx++] = tmp;
+        }
+    }
 }
