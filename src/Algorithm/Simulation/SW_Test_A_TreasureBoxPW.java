5c6f05a075f6ff9938fa632a8cc254dde8868cc9
who._.hs <108789695+ChoeHyeongSoo@users.noreply.github.com>
2026-02-28 16:24:26 +0900

    2026 Feb 4th week [SW_Test_A_TreasureBoxPW] - Rotation: Used LinkedList to efficiently handle the circular shift - Using HashSet to prevent duplication - Hex Conversion: BitMasking (1<<4*k)

--- src/Algorithm/Simulation/SW_Test_A_TreasureBoxPW.java
+++ src/Algorithm/Simulation/SW_Test_A_TreasureBoxPW.java
@@ -6,8 +6,13 @@ import java.util.*;
 
 class SW_Test_A_TreasureBoxPW {
     /*
-
-     */
+    각 변에 16진수 숫자(0~F)가 적혀 있는 보물상자가 있다.
+    보물 상자의 뚜껑은 시계방향으로 돌릴 수 있고, 한 번 돌릴 때마다 숫자가 시계방향으로 한 칸씩 회전한다.
+    각 변에는 동일한 개수의 숫자가 있고, 시계방향 순으로 높은 자리 숫자에 해당하며 하나의 수를 나타낸
+    보물상자에는 자물쇠가 걸려있는데, 이 자물쇠의 비밀번호는 보물 상자에 적힌 숫자로 만들 수 있는 모든 수 중, K번째로 큰 수를 10진 수로 만든 수이다.
+    N개의 숫자가 입력으로 주어졌을 때, 보물상자의 비밀 번호를 출력하는 프로그램을 만들어보자.
+    (서로 다른 회전 횟수에서 동일한 수가 중복으로 생성될 수 있다. 크기 순서를 셀 때 같은 수를 중복으로 세지 않도록 주의한다.)
+    */
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
