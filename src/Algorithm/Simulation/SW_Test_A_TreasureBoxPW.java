e27ec7e59bbb1b62f818e42f312df822e83d52ab
who._.hs <108789695+ChoeHyeongSoo@users.noreply.github.com>
2026-02-28 16:22:30 +0900

    2026 Feb 4th week [SW_Test_A_TreasureBoxPW] - Rotation: Used LinkedList to efficiently handle the circular shift - Using HashSet to prevent duplication - Hex Conversion: BitMasking (1<<4*k)

--- src/Algorithm/Simulation/SW_Test_A_TreasureBoxPW.java
+++ src/Algorithm/Simulation/SW_Test_A_TreasureBoxPW.java
@@ -1,15 +1,16 @@
+package Algorithm.Simulation;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
-class Solution {
+class SW_Test_A_TreasureBoxPW {
     /*
 
      */
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-
         for (int test_case = 1; test_case <= T; test_case++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
@@ -20,11 +21,11 @@ class Solution {
             LinkedList<Character> arr = new LinkedList<>();
             for (int i = 0; i < n; i++)
                 arr.add(sb.charAt(i));
-            // 순위-중복 금지 -> 세트에 저장해서 내림차순 정렬
+            // 순위-중복 금지 -> 세트에 저장 - 정렬
             Set<String> pw = new HashSet<>(); // 배열에 옮기며 정렬
             int w = n/4; // pw 길이
             save(arr, pw, w);
-            for (int i = 0; i < w; i++)  // 3번 회전
+            for (int i = 0; i < w; i++)  // w번 회전
                 rotate_save(arr, pw, w);
 
             // 정렬 및 k번째 반환
@@ -64,13 +65,13 @@ class Solution {
 
     // 16->2진수 ; 각 자리 8^i * arr[i]
     public static void convert(Set<String> pw, int[] arr) {
-        // c - '0', A~F는 - 'A' +10 : "ABCEF".contains(c) <-valueOf(c)?
+        // c - '0', A~F는 - 'A' +10 : "ABCEF".contains(c) <-valueOf(c)? <- D를 빼먹어서 2시간동안 디버깅..
         int idx = 0;
         for (String s : pw) {
             int tmp = 0;
             for (int i = 0; i < s.length(); i++) {
                 char c = s.charAt(i);
-                if ("ABCEF".contains(String.valueOf(c)))
+                if ("ABCDEF".contains(String.valueOf(c)))
                     tmp += (c - 'A' + 10)*(1<<(4*(s.length()-1-i)));
                 else tmp += (c - '0')*(1<<(4*(s.length()-1-i)));
             }
