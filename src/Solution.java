e01d74d57ec5980558382f9fb7cb0f8c4c5a2cb8
who._.hs <108789695+ChoeHyeongSoo@users.noreply.github.com>
2026-03-02 20:40:21 +0900

    Update Basic Template

--- src/Solution.java
+++ src/Solution.java
@@ -9,7 +9,6 @@ class Solution {
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-
         for (int test_case = 1; test_case <= T; test_case++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
