fbddaaf4689297138b7fbbd57147dbb5db6a595a
ChoeHyeongSoo <sam000208@naver.com>
2026-03-04 13:54:41 +0900

    Update Basic Template

--- src/Solution.java
+++ src/Solution.java
@@ -5,16 +5,20 @@ import java.util.*;
 class Solution {
     /*
 
-     */
+    */
     public static void main(String args[]) throws Exception {
+        StringBuilder ans = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         for (int test_case = 1; test_case <= T; test_case++) {
+            ans.append("#").append(test_case).append(" ");
             StringTokenizer st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
 
-//            System.out.println("#" + test_case + " " + );
-        }
-    }
 
+
+            ans.append('\n');
+        }
+        System.out.print(ans);
+    }
 }
