cdf510b09db118caf243ef632086575e1d19a0f2
who._.hs <108789695+ChoeHyeongSoo@users.noreply.github.com>
2026-03-10 23:37:51 +0900

    [D5] Title: 상원이의 생일파티, Time: 238 ms, Memory: 42,112 KB -BaekjoonHub

--- SWEA/D5/5521. 상원이의 생일파티/상원이의 생일파티.java
+++ SWEA/D5/5521. 상원이의 생일파티/상원이의 생일파티.java
@@ -12,7 +12,7 @@ class Solution {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
-            List<Integer>[] friends = new ArrayList[n+1];
+            List<Integer>[] friends = new ArrayList[n + 1];
             for (int i = 1; i <= n; i++)
                 friends[i] = new ArrayList<>();
             for (int i = 0; i < m; i++) {
@@ -23,29 +23,23 @@ class Solution {
             }
 
             int cnt = 0;
-            boolean[] v = new boolean[n+1];
+            boolean[] v = new boolean[n + 1];
             if (!friends[1].isEmpty()) {
-
-                Deque<Integer> q = new ArrayDeque<>();
-
-                for (int f : friends[1]) q.offer(f);
                 v[1] = true;
-
-                while (!q.isEmpty()) {
-
-                    int curr = q.poll();
-                    v[curr] = true;
-
-                    for (int f : friends[curr]) {
-                        if (v[f]) continue;
+                List<Integer> direct = friends[1];
+                for (int f : direct) {
+                    if (!v[f]) {
                         v[f] = true;
+                        cnt++;
+                    }
+                    for (int ff : friends[f]) {
+                        if (!v[ff]) {
+                            v[ff] = true;
+                            cnt++;
+                        }
                     }
                 }
             }
-
-            for (int i = 2; i <= n; i++)
-                if (v[i]) cnt++;
-
             ans.append(cnt).append('\n');
         }
         System.out.print(ans);
