5a28f612f56af8f3f711fba4aac0b86cd9c7bf13
who._.hs <108789695+ChoeHyeongSoo@users.noreply.github.com>
2026-03-13 15:01:52 +0900

    [D2] Title: 쉬운 거스름돈, Time: 109 ms, Memory: 27,520 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
@@ -6,24 +6,44 @@ class Solution {
     public static void main(String args[]) throws Exception {
         StringBuilder ans = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine().trim());
         for (int test_case = 1; test_case <= T; test_case++) {
             ans.append("#").append(test_case).append('\n');
-            int n = Integer.parseInt(br.readLine());
-            int[] money = new int[8];
-            int div = 50000;
-            money[0] = n/div;
-            for (int i = 1; i < 8; i++) {
-                n -= money[i-1]*div;
-                if (i%2==1) div /= 5;
-                else div /= 2;
-                money[i] = n/div;
+            int n = Integer.parseInt(br.readLine().trim()) / 10;
+            int[] money = new int[n+1];
+            int[] use = new int[n+1];
+
+            Arrays.fill(money, Integer.MAX_VALUE);
+            money[0] = 0;
+            use[0] = 0;
+
+            for (int i = 1; i <= n; i++) {
+                for (int c : each) {
+                    if (i < c) continue;
+                    if (money[i-c]==Integer.MAX_VALUE) continue;
+                    if (money[i] >= money[i-c] + 1) {
+                        money[i] = money[i-c] + 1;
+                        use[i] = c;
+                    }
+                }
             }
 
-            for (int k : money) ans.append(k).append(" ");
+//            Map<Integer, Integer> cnt = new HashMap<>();
+//            for (int c : each) cnt.put(c, 0);
+            int[] cnt = new int[5001];
+            int idx = n;
+            while (idx > 0) {
+//                cnt.put(use[idx], cnt.get(use[idx])+1);
+                cnt[use[idx]]++;
+                idx-=use[idx];
+            }
+
+            for (int i = 7; i >= 0; i--) ans.append(cnt[each[i]]).append(" ");
 
             ans.append('\n');
         }
         System.out.print(ans);
     }
+
+    static int[] each = {1, 5, 10, 50, 100, 500, 1000, 5000};
 }
