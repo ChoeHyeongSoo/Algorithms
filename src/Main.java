1fb4535363bd5b75f818396230289b78c2b327ea
ChoeHyeongSoo <sm000208@naver.com>
2026-03-20 17:57:51 +0900

    11723 Ing

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,42 @@ public class Main{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int v = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
+        Mission[] operators = new Mission[m];
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            operators[i] = new Mission(st.nextToken(), Integer.parseInt(st.nextToken()));
+        }
 
+
+
+    }
+    static StringBuilder sb;
+    public static void operation(Mission tmp) {
+
+        switch (tmp.op) {
+            case "add":
+                sb.append(tmp.n);
+                break;
+            case "remove":
+
+                break;
+            case "add":
+                sb.append(tmp.n);
+                break;
+        }
+
+    }
+
+}
+
+class Mission {
+    String op;
+    int n;
+
+    public Mission(String op, int n) {
+        this.op = op;
+        this.n = n;
     }
 }
