61415e44921133f6010fd666efad100d51cfb509
who._.hs <108789695+ChoeHyeongSoo@users.noreply.github.com>
2026-03-20 21:11:53 +0900

    BaekJoon Main Template

--- src/Main.java
+++ src/Main.java
@@ -7,42 +7,11 @@ public class Main{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken());
-        sb = new StringBuilder();
-        Mission[] operators = new Mission[m];
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            operators[i] = new Mission(st.nextToken(), Integer.parseInt(st.nextToken()));
-        }
+        n = Integer.parseInt(st.nextToken());
 
 
 
     }
-    static StringBuilder sb;
-    public static void operation(Mission tmp) {
+    static int n;
 
-        switch (tmp.op) {
-            case "add":
-                sb.append(tmp.n);
-                break;
-            case "remove":
-
-                break;
-            case "add":
-                sb.append(tmp.n);
-                break;
-        }
-
-    }
-
-}
-
-class Mission {
-    String op;
-    int n;
-
-    public Mission(String op, int n) {
-        this.op = op;
-        this.n = n;
-    }
 }
