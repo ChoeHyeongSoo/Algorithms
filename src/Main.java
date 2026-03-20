1061d8e13f510a783d65b8b34015e2e96e3f04d3
who._.hs <108789695+ChoeHyeongSoo@users.noreply.github.com>
2026-03-21 00:11:15 +0900

    BaekJoon Main Template

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
 public class Main{
     public static void main(String[] args) throws Exception {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
@@ -13,5 +11,4 @@ public class Main{
 
     }
     static int n;
-
 }
