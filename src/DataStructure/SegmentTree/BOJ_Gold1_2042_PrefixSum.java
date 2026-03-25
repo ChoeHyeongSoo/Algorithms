a7a3edd568cb49e49e7a5c52acbc97fa23808183
who._.hs <108789695+ChoeHyeongSoo@users.noreply.github.com>
2026-03-25 23:03:58 +0900

    2026 Mar 4th Week [BOJ_Gold1_2042] - Prefix sum solved with Segment Tree using Top-Down & Bottom-Up(bit operation)

--- src/DataStructure/SegmentTree/BOJ_Gold1_2042_PrefixSum.java
+++ src/DataStructure/SegmentTree/BOJ_Gold1_2042_PrefixSum.java
@@ -1,7 +1,9 @@
+package DataStructure.SegmentTree;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-public class PrefixSum{
+public class BOJ_Gold1_2042_PrefixSum {
     public static void main(String[] args) throws Exception {
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
