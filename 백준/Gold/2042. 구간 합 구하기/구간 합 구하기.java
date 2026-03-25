016bffcb7bdb12392d1152862f71b820ba0d60c0
who._.hs <108789695+ChoeHyeongSoo@users.noreply.github.com>
2026-03-25 22:57:41 +0900

    [Gold I] Title: 구간 합 구하기, Time: 468 ms, Memory: 116384 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -3,34 +3,106 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 public class Main{
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
         arr = new long[n];  // k번째처럼 인덱스 위주의 문제는 n+1로 설정하고, 트리 생성할 때도 1~n으로 범위 잡아주는 게 편리
         for (int i = 0; i < n; i++) arr[i] = Long.parseLong(br.readLine());
-        SegmentTree prefix_sum = new SegmentTree(arr);
+//        SegmentTree prefix_sum = new SegmentTree(arr);
+        prefix_sum = new SegmentTree_BottomUp(arr);
+//        top_down();
+        bottom_up();
+        System.out.println(sb);
+    }
+
+    static int n, m, k;
+    static long[] arr;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+//    static SegmentTree prefix_sum;
+    static SegmentTree_BottomUp prefix_sum;
+
+//    public static void top_down() throws Exception{
+//        for (int i = 0; i < m+k; i++) {
+//            st = new StringTokenizer(br.readLine());
+//            int option = Integer.parseInt(st.nextToken()), target = Integer.parseInt(st.nextToken());
+//            switch (option) {
+//                case 1:
+//                    long change = Long.parseLong(st.nextToken()); // 주의! 변한값이 아니라 변화량을 반영해야 한다.
+//                    long diff = change - arr[target-1]; // 변화량 계산 (Top-Down)
+//                    arr[target-1] = change;     // *원본 배열에도 변한값 업데이트
+//                    prefix_sum.update(1, 0, n-1, target-1, diff); // x가 아니라 dx! - 트리의 값 변경 (Top-Down)
+//                    break;
+//                case 2:
+//                    int last = Integer.parseInt(st.nextToken());
+//                    sb.append(prefix_sum.sum(1, 0, n-1, target-1, last-1)).append('\n');
+//                    break;
+//            }
+//        }
+//    }
+
+    public static void bottom_up() throws Exception{
         for (int i = 0; i < m+k; i++) {
             st = new StringTokenizer(br.readLine());
             int option = Integer.parseInt(st.nextToken()), target = Integer.parseInt(st.nextToken());
             switch (option) {
                 case 1:
-                    long change = Long.parseLong(st.nextToken()); // 주의! 변한값이 아니라 변화량을 반영해야 한다.
-                    long diff = change - arr[target-1]; // 변화량 계산
-                    arr[target-1] = change;     // 원본 배열에도 변한값 할당
-                    prefix_sum.update(1, 0, n-1, target-1, diff); // x가 아니라 dx! - 트리의 값 변경
+                    long change = Long.parseLong(st.nextToken());
+                    arr[target-1] = change;     // *원본 배열에도 변한값 업데이트
+                    prefix_sum.update(target-1, change);
                     break;
                 case 2:
                     int last = Integer.parseInt(st.nextToken());
-                    sb.append(prefix_sum.sum(1, 0, n-1, target-1, last-1)).append('\n');
+                    sb.append(prefix_sum.sum(target-1, last-1)).append('\n');
                     break;
             }
         }
-        System.out.println(sb);
     }
-    static int n;
-    static long[] arr;
+
+}
+
+class SegmentTree_BottomUp{
+
+    long[] tree;
+    int n;
+
+    public SegmentTree_BottomUp(long[] arr){
+        n = arr.length;
+        tree = new long[n*2+1];
+        build(arr);
+    }
+
+    public void build(long[] arr) {
+        for (int i = 0; i < n; i++) tree[i+n] = arr[i]; // 리프 노드 먼저 할당
+
+        for (int i = n-1; i >= 1; i--) tree[i] = tree[i << 1] + tree[i << 1 | 1]; // 부모 노드 채우기
+    }
+
+    public long sum(int left, int right) {
+        long result = 0;
+        left += n; right += n;
+
+        while (left<=right) {
+            if (left % 2 == 1) result += tree[left++];      // left 홀수 : 현재 노드의 부모 노드는 탐색 범위 밖의 값을 포함한 누적합
+            if (right % 2 == 0) result += tree[right--];    // right 짝수 : ''
+
+            left >>= 1;
+            right >>= 1;
+        } return result;
+    }
+
+    public void update(int idx, long value) {
+        idx+=n;
+        tree[idx] = value;
+
+        while(idx>1) {  // 루트까지 업데이트값 전파 (Bottom-Up)
+            idx >>= 1;
+            tree[idx] = tree[idx << 1] + tree[idx << 1 | 1];
+        }
+    }
+
 }
 
 class SegmentTree {
@@ -40,40 +112,41 @@ class SegmentTree {
     public SegmentTree(long[] arr) {
         n = arr.length;
         tree = new long[n*4];
-        build(arr, 1, 0, n-1);
+        build(arr, 1, 0, n-1);  // 분할 정복으로 트리 형성
     }
 
     public long build(long[] arr, int node, int start, int end) {
 
-        if (start==end) return tree[node]=arr[start];
+        if (start==end) return tree[node]=arr[start];   // 리프 노드 조건
 
-        int mid = (start+end)/2;
+        int mid = (start+end)/2;    // 분할 기준
 
-        return tree[node] = build(arr, node * 2, start, mid) + build(arr, node * 2 + 1, mid+1, end);
+        return tree[node] = build(arr, node * 2, start, mid)    // 좌/우 자식 노드 분할 정복 : 리턴하며 병합
+                + build(arr, node * 2 + 1, mid+1, end);
     }
 
     public long sum(int node, int start, int end, int left, int right) {
 
-        if (left > end || right < start) return 0;
+        if (left > end || right < start) return 0;  // 구간 밖: 연산에 영향 없는 값 리턴
 
-        if (left <= start && end <= right) return tree[node];
+        if (left <= start && end <= right) return tree[node];  // 완전 포함: 노드 값 전체 리턴 (분할 x)
 
         int mid = (start+end)/2;
 
-        return sum(node*2, start, mid, left, right)
+        return sum(node*2, start, mid, left, right) // 일부 포함: 분할 정복
                 + sum(node*2+1, mid+1, end, left, right);
     }
 
     public void update(int node, int start, int end, int idx, long diff) {
 
-        if (start > idx || end < idx) return;
+        if (start > idx || end < idx) return;   // 구간 밖: 종료
 
-        tree[node]+=diff;
+        tree[node]+=diff;   // 구간에 포함되면 현재 노드부터 업데이트
 
-        if (start==end) return;
+        if (start==end) return; // 리프면 분할 x
 
         int mid = (start+end)/2;
-        update(node*2, start, mid, idx, diff);
+        update(node*2, start, mid, idx, diff);  // 서브 트리 전체 업데이트
         update(node*2+1, mid+1, end, idx, diff);
     }
 }
