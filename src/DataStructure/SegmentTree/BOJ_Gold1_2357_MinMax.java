3576c1e158ae190a02ebe55a8768209ea9cd26c1
ChoeHyeongSoo <sam000208@naver.com>
2026-03-26 17:47:34 +0900

    2026 Mar 4th Week [BOJ_Gold1_2357_MinMax] - Solve range min & max using Segment Tree to understand how to use segment tree exactly

---
+++ src/DataStructure/SegmentTree/BOJ_Gold1_2357_MinMax.java
@@ -0,0 +1,77 @@
+package DataStructure.SegmentTree;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_Gold1_2357_MinMax{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(st.nextToken()); int m = Integer.parseInt(st.nextToken());
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++) arr[i] = Long.parseLong(br.readLine());
+        SegmentTree_MinMax segment_tree = new SegmentTree_MinMax(arr);
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+            sb.append(segment_tree.min(a-1, b-1)).append(" ").append(segment_tree.max(a-1, b-1)).append('\n'); // Index임을 주의!
+        }
+        System.out.println(sb);
+    }
+    static int n;
+}
+
+class SegmentTree_MinMax {
+    int n;
+    long[][] tree;
+
+    public SegmentTree_MinMax(long[] arr) {
+        n = arr.length;
+        tree = new long[n*2][2];
+        build(arr);
+    }
+
+    public void build(long[] arr) {
+
+        for (int i = 0; i < n; i++) {
+            tree[i+n][0] = arr[i]; // 리프 노드 우선 할당
+            tree[i+n][1] = arr[i];
+        }
+
+
+        for (int i = n-1; i > 0; i--) {
+            tree[i][0] = Math.min(tree[i<<1][0], tree[i<<1|1][0]); // 최소값 계산
+            tree[i][1] = Math.max(tree[i<<1][1], tree[i<<1|1][1]);
+        }
+    }
+
+    public long min(int left, int right) {
+
+        long result = Integer.MAX_VALUE;
+        left+=n; right+=n;
+        while (left<=right) {
+            if (left % 2 == 1) result = Math.min(result, tree[left++][0]);     // 최소값 계산후, 올바른 부모 노드 찾아가도록
+            if (right % 2 == 0) result =  Math.min(result, tree[right--][0]);
+
+            left >>= 1;
+            right >>= 1;
+
+        } return result;
+    }
+
+    public long max(int left, int right) {
+
+        long result = 0;
+        left+=n; right+=n;
+        while (left<=right) {
+            if (left % 2 == 1) result = Math.max(result, tree[left++][1]);     // 최대값 계산후, 올바른 부모 노드 찾아가도록
+            if (right % 2 == 0) result =  Math.max(result, tree[right--][1]);
+
+            left >>= 1;
+            right >>= 1;
+
+        } return result;
+    }
+}
