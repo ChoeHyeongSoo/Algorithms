bad061cb7e297916e7a139872b1555c758f0ad79
who._.hs <108789695+ChoeHyeongSoo@users.noreply.github.com>
2026-03-30 11:57:05 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 192 ms, Memory: 45180 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.StringTokenizer;
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[101];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()), y= Integer.parseInt(st.nextToken());
+            map[x] = y;
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken()), v = Integer.parseInt(st.nextToken());
+            map[u] = v;
+        }
+
+        // BFS
+        int[] visit = new int[101];
+        Arrays.fill(visit, 100);
+        Deque<Integer> q = new ArrayDeque<>();
+        Deque<Integer> in = new ArrayDeque<>();
+        q.offer(1);
+        int cnt = 0;
+        main_loop:
+        while (true) {
+            while (!q.isEmpty()) {
+
+                int curr = q.poll();
+                if (curr == 100) break main_loop;
+
+                for (int i = 6; i >= 1; i--) {
+                    int next = curr + i;
+
+                    if (next > 100 || visit[next] < cnt) continue;
+                    visit[next] = Math.min(visit[next], cnt);
+                    if (map[next] > 0) {
+                        next = map[next];
+                        visit[next] = cnt;
+                    }
+                    in.offer(next);
+                }
+            }
+            cnt++;
+            while (!in.isEmpty()) q.offer(in.poll());
+        }
+        System.out.println(cnt);
+    }
+    static int n, m;
+    static int[] map;
+}
