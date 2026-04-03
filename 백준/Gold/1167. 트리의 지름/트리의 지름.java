d08b57e77da65ee8cf4c08191c457b77efdd82ef
who._.hs <108789695+ChoeHyeongSoo@users.noreply.github.com>
2026-04-03 23:54:51 +0900

    [Gold II] Title: 트리의 지름, Time: 740 ms, Memory: 107824 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        v = Integer.parseInt(st.nextToken());
+        adj = new ArrayList[v+1];
+        for (int i = 1; i <= v; i++) adj[i] = new ArrayList<>();
+        for (int i = 0; i < v; i++) {
+            st = new StringTokenizer(br.readLine());
+            int curr = Integer.parseInt(st.nextToken());
+            int target = 0;
+           while (true) {
+               target = Integer.parseInt(st.nextToken());
+               if (target==-1) break;
+               int dist = Integer.parseInt(st.nextToken());
+               adj[curr].add(new Node(target, dist));
+           }
+        }
+        dist = new int[v+1];
+        Arrays.fill(dist, -1);
+        l = 0; idx = 0;
+        bfs(1);
+        l = 0;
+        dist = new int[v+1];
+        Arrays.fill(dist, -1);
+        bfs(idx);
+        System.out.println(l);
+    }
+    static int v, l, idx;
+    static int[] dist;
+    static List<Node>[] adj;
+
+    static void bfs(int tmp) {
+
+        Deque<Integer> q = new ArrayDeque<>();
+        q.offer(tmp); dist[tmp] = 0;
+        while (!q.isEmpty()) {
+
+            int curr = q.poll();
+
+            for (Node next : adj[curr]) {
+                if (dist[next.v]>-1) continue;
+                dist[next.v] = dist[curr] + next.dist;
+                if (l < dist[curr] + next.dist) {
+                    l = dist[curr] + next.dist;
+                    idx = next.v;
+                }
+                q.offer(next.v);
+            }
+        }
+    }
+}
+
+class Node {
+    int v, dist;
+
+    public Node(int v, int dist) {
+        this.v = v;
+        this.dist = dist;
+    }
+}
