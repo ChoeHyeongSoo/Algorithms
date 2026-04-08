35b941e208f521a95881351bd3eba45084c9b833
who._.hs <108789695+ChoeHyeongSoo@users.noreply.github.com>
2026-04-08 15:34:27 +0900

    [Gold III] Title: 게리맨더링, Time: 88 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        p = new int[n+1];
+        adj = new ArrayList[n+1];
+        int total = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            p[i] = Integer.parseInt(st.nextToken());
+            total+=p[i];
+            adj[i] = new ArrayList<>();
+        }
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int len = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < len; j++) adj[i].add(Integer.parseInt(st.nextToken()));
+        }
+
+        int ans = Integer.MAX_VALUE;
+        // 비트마스킹 : 부분 집합을 대체하라.
+        for (int bit = 1; bit < (1<<n)-1; bit++) { // 0이거나 N인 경우는 선거구 조건 불충족
+            // 연결 여부 체크
+            boolean group_a = is_connected(bit), group_b = is_connected(((1<<n)-1)^bit);
+            // 연결이라면 인구수 카운팅 - 전체 인구수 - 비트마스킹 구연 인구수 * 2
+            if (!(group_a&&group_b)) continue;
+            int population = 0;
+            for (int i = 1; i <= n; i++) if ((bit&(1<<(i-1)))!=0) population+=p[i];
+            ans = Math.min(ans, Math.abs(total - 2*population));
+        }
+        System.out.println(ans!=Integer.MAX_VALUE ? ans : -1);
+    }
+    static int n;
+    static int[] p;
+    static List<Integer>[] adj;
+
+    static boolean is_connected(int bit) {
+
+        int threshold = Integer.bitCount(bit), init = Integer.numberOfTrailingZeros(bit & (-bit))+1;
+
+        Deque<Integer> q = new ArrayDeque<>();
+        boolean[] visit = new boolean[n+1];
+        visit[init]=true; q.offer(init);
+        int cnt = 1;
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+
+            for (int k : adj[curr]) {
+
+                if ((bit&(1<<(k-1)))==0) continue;
+                if (visit[k]) continue;
+
+                visit[k] = true;
+                q.offer(k);
+                cnt++;
+            }
+        }
+        return cnt == threshold;
+    }
+}
