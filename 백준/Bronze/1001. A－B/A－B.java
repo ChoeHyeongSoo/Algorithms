856e13ac51c62e9ce80695d50b238f366ec9b4c9
who._.hs <108789695+ChoeHyeongSoo@users.noreply.github.com>
2026-04-10 13:09:12 +0900

    [Bronze V] Title: A-B, Time: 72 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        System.out.println(n-m);
+    }
+    static int n, m;
+}
