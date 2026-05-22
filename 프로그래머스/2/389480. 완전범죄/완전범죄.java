5daa2a4bef951bdd9a53fa7a668ecdd88b1d7685
who._.hs <108789695+ChoeHyeongSoo@users.noreply.github.com>
2026-05-22 10:47:43 +0900

    [level 2] Title: 완전범죄, Time: 0.41 ms, Memory: 63.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[][] info, int n, int m) {
+        // Top-Down DP : Backtracking - Threshold (n, m) - 불가능 존재
+        // A가 최소가 되게 memoization
+        INFO = info;
+        table = new int[info.length+1][m+1];
+        for (int[] r : table)
+            Arrays.fill(r, -1);
+
+        int ans = dp(info.length-1, m);
+        return ans < n ? ans : -1; // INF 가지고 돌아오면 불가능
+    }
+
+    static final int INF = 100000;
+    static int[][] INFO, table;
+
+    public int dp(int i, int b) {
+
+        if ( b<=0) return INF; // A case: n, B case: m 이상 - INF 가지고 리턴
+
+        // *기저조건 : 가지치기 없이 도달하면 여기서부터 값을 더하며 복귀
+        if (i==-1) return 0; // 0부터 INFO[i][0] 값이 더해지므로, 0 반환
+
+        if (table[i+1][b]!=-1 && table[i+1][b]!=INF) return table[i+1][b];
+
+        int result = dp(i-1, b-INFO[i][1]);
+
+            result = Math.min(result, dp(i-1, b) + INFO[i][0]);
+
+        return table[i+1][b] = result;
+    }
+}
